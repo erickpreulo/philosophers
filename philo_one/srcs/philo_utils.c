989992ef59232a433eb0434489585fa87cb29c06
Erick Gomes <erickgomes@Mac-mini-de-Erick.local>
2021-11-08 18:33:02 +0000

    done

---
+++ philo_one/srcs/philo_utils.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egomes <egomes@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/18 17:26:22 by egomes            #+#    #+#             */
+/*   Updated: 2021/11/05 17:27:52 by egomes           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philo.h"
+
+void	init_mutex(t_obj *obj)
+{
+	int	i;
+
+	i = 0;
+	while (i < obj->arg.philos)
+		pthread_mutex_init(&(obj->arg.forks[i++]), NULL);
+	pthread_mutex_init(&(obj->arg.writing), NULL);
+	pthread_mutex_init(&(obj->arg.meal_check), NULL);
+}
+
+void	init_philos(t_obj *obj)
+{
+	int	i;
+
+	i = 0;
+	while (i < obj->arg.philos)
+	{
+		obj->philo[i].id = i;
+		obj->philo[i].l_f = i;
+		obj->philo[i].r_f = (i + 1) % obj->arg.philos;
+		obj->philo[i].last_meal = 0;
+		obj->philo[i].arg = &obj->arg;
+		i++;
+	}
+}
+
+void	exit_function(t_obj *obj)
+{
+	int	i;
+
+	i = -1;
+	while (++i < obj->arg.philos)
+		pthread_join(obj->philo[i].thread_id, NULL);
+	i = -1;
+	while (++i < obj->arg.philos)
+		pthread_mutex_destroy(&(obj->arg.forks[i]));
+	pthread_mutex_destroy(&(obj->arg.writing));
+}
---
+++ philo_one/srcs/utils.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egomes <egomes@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/08 05:10:12 by egomes            #+#    #+#             */
+/*   Updated: 2021/11/08 17:46:32 by egomes           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int	neg;
+	int	i;
+	int	num;
+
+	i = 0;
+	neg = 1;
+	num = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		num = num * 10 + (str[i] - 48);
+		i++;
+	}
+	return (num * neg);
+}
+
+long int	actual_time(void)
+{
+	long int		time;
+	struct timeval	current_time;
+
+	time = 0;
+	if (gettimeofday(&current_time, NULL) == -1)
+		return (error("Gettimeofday returned -1"));
+	time = (current_time.tv_sec * 1000) + (current_time.tv_usec / 1000);
+	return (time);
+}
+
+void	print(t_arg *arg, int id, char *str)
+{
+	pthread_mutex_lock(&(arg->writing));
+	if (!(arg->dieded))
+	{
+		printf("%lli ", actual_time() - arg->first_time);
+		printf("%i ", id + 1);
+		printf("%s\n", str);
+	}
+	pthread_mutex_unlock(&(arg->writing));
+}
+
+void	smart_sleep(long long time, t_arg *arg)
+{
+	long long	i;
+
+	i = actual_time();
+	while (!(arg->dieded))
+	{
+		if ((actual_time() - i) >= time)
+			break ;
+		usleep(50);
+	}
+}
